4f3fe16c8d3f0b3e432fd95b680e03416a714bb0
Aden0020 <aden0020@hotmail.com>
2025-11-18 13:43:27 +0100

    service

--- src/main/java/com/aljamour/pkveksamen/Service/ProjectService.java
+++ src/main/java/com/aljamour/pkveksamen/Service/ProjectService.java
@@ -1,4 +1,12 @@
 package com.aljamour.pkveksamen.Service;
 
+import com.aljamour.pkveksamen.Repository.ProjectRepository;
+
 public class ProjectService {
+
+    private ProjectRepository projectRepository;
+
+    public ProjectService (ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 }
--- src/main/java/com/aljamour/pkveksamen/Service/UserService.java
+++ src/main/java/com/aljamour/pkveksamen/Service/UserService.java
@@ -1,6 +1,37 @@
 package com.aljamour.pkveksamen.Service;
 
+import com.aljamour.pkveksamen.Repository.UserRepository;
+import org.apache.catalina.User;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
+
 public class UserService {
-    //ggkk
-// gaf
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
+
+//    public boolean createUser(String userName, String email, String userPassword, String role) {
+//        try {
+//            userRepository.createUser(userName, email, userPassword, role);
+//            System.out.println("Bruger oprettet: " + userName + " " + email);
+//            return true;
+//        } catch (DuplicateKeyException e) {
+//            System.out.println("Email allerede i brug: " + email);
+//            e.printStackTrace();
+//            return false;
+//        } catch (Exception e) {
+//            System.out.println("Uventet fejl ved oprettelse af bruger: " + e.getMessage());
+//            e.printStackTrace();
+//            return false;
+//        }
+//
+//    }
+//     public int validateLogin(String username, String userPassword) {
+//        int id = 0;
+//        id = userRepository.validateLogin(username, userPassword);
+//        return id;
+//    }
+//}
