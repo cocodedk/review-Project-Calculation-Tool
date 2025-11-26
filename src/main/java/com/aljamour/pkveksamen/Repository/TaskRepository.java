f797a933c6b44e311a7ec943a0c06ea4b2c24f97
Aden0020 <aden0020@hotmail.com>
2025-11-26 12:04:39 +0100

    change

--- src/main/java/com/aljamour/pkveksamen/Repository/TaskRepository.java
+++ src/main/java/com/aljamour/pkveksamen/Repository/TaskRepository.java
@@ -15,22 +15,23 @@ public class TaskRepository {
     public TaskRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-}
 
-//    public List<Task> getTasksByTaskID(int taskID) {
-//
-//        String sql = "SELECT task_id AS taskID," +
-//                "  user_id AS " +
-//                "  sub_project_id " +
-//                "  task_title AS taskTitle" +
-//                "  task_description AS taskDescription" +
-//                "  task_status AS taskStatus" +
-//                "  task_start_date AS startDate" +
-//                "  task_end_date AS endDate" +
-//                "  task_duration AS duration" +
-//                "  task_priority AS taskPriority" +
-//                "  task_note AS taskNote";
-////                "FROM task WHERE ID = ?";
-////                return jdbcTemplate-query(sql, new BeanPropertyRowMapper<>(Task.class), taskID);
-//    }
-//}
+
+
+
+
+    public List<Task> getTasksByTaskID(int taskID) {
+
+        String sql = "SELECT task_id AS taskID," +
+                "  task_description AS taskDescription" +
+                "  task_status AS taskStatus" +
+                "  task_start_date AS startDate" +
+                "  task_end_date AS endDate" +
+                "  task_duration AS duration" +
+                "  task_priority AS taskPriority" +
+                "  task_note AS taskNote" +
+                "FROM task WHERE task_id = ?";
+                return jdbcTemplate-query(sql, new BeanPropertyRowMapper<>(Task.class), taskID);
+
+    }
+}
